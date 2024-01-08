cba86bad668aadfb80d3aa69de52db57806b7e53
DinhTrungQuy <quythks2023@gmail.com>
2024-01-08 16:02:36 +0700

    Update package.json and action.ts files

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,9 +1,20 @@
 'use server';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerID: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerID, amount, status } = CreateInvoice.parse({
     customerID: formData.get('customerID'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
   console.log(rawFormData);
 }
